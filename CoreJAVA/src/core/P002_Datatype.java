52d422a0919b2aafa1d075c345dead34a1a1ba50
jay-java <jayprajapati.tops@gmail.com>
2025-08-13 15:52:07 +0530

    operators typecasting

--- CoreJAVA/src/core/P002_Datatype.java
+++ CoreJAVA/src/core/P002_Datatype.java
@@ -3,8 +3,11 @@ package core;
 public class P002_Datatype {
 	public static void main(String[] args) {
 		byte b = 12;// 1
+		System.out.println(b);
 		short s = 32767;// 2
+		System.out.println("value of short s = "+s);
 		int i = 12;// 4
+		System.out.println("value of int i = "+i);
 		long l = 12;// 8
 
 		float f = 3.14f;// 4
